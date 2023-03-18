1e708d85dc5661888d03716ac25b761c585218e9
GabrielRodriguesMachado <gabrm123@gmail.com>
2023-03-18 16:25:03 -0300

    criando os components do chat

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import socketIO from 'socket.io-client';
+import ChatPage from './components/ChatPage';
 import Home from './components/Home';
 
 const socket = socketIO.connect('http://localhost:4000');
@@ -9,6 +10,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home socket={socket} />} />
+        <Route path="/chat" element={<ChatPage socket={socket} />} />
       </Routes>
     </BrowserRouter>
   );
